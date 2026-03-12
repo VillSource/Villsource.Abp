ede886cc7a6f18c1134f7a0fa5efc20d168e24ef
VillSource <30929839+VillSource@users.noreply.github.com>
2026-03-12 23:57:59 +0700

    feat: add primeng

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -3,14 +3,14 @@ import { provideAbpCore, withOptions } from '@abp/ng.core';
 import { provideAbpOAuth } from '@abp/ng.oauth';
 import { provideSettingManagementConfig } from '@abp/ng.setting-management/config';
 import { provideFeatureManagementConfig } from '@abp/ng.feature-management';
-import { provideAbpThemeShared,} from '@abp/ng.theme.shared';
+import { provideAbpThemeShared } from '@abp/ng.theme.shared';
 import { provideIdentityConfig } from '@abp/ng.identity/config';
 import { provideAccountConfig } from '@abp/ng.account/config';
 import { provideTenantManagementConfig } from '@abp/ng.tenant-management/config';
 import { registerLocaleForEsBuild } from '@abp/ng.core/locale';
 import { provideThemeLeptonX } from '@abp/ng.theme.lepton-x';
 import { provideSideMenuLayout } from '@abp/ng.theme.lepton-x/layouts';
-import { provideLogo, withEnvironmentOptions } from "@abp/ng.theme.shared";
+import { provideLogo, withEnvironmentOptions } from '@abp/ng.theme.shared';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
@@ -18,6 +18,57 @@ import { environment } from '../environments/environment';
 import { APP_ROUTES } from './app.routes';
 import { APP_ROUTE_PROVIDER } from './route.provider';
 import { FOOTER_PROVIDER } from './footer/footer.config';
+import { providePrimeNG } from 'primeng/config';
+import { definePreset, palette } from '@primeuix/themes';
+import Aura from '@primeuix/themes/material';
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#fcfcfc',
+      100: '#f5f5f5',
+      200: '#e5e5e5',
+      300: '#d4d4d4',
+      400: '#a3a3a3',
+      500: '#000000', // The primary color
+      600: '#000000',
+      700: '#000000',
+      800: '#000000',
+      900: '#000000',
+      950: '#000000',
+    },
+    colorScheme: {
+      light: {
+        primary: {
+          color: '{primary.500}',
+          inverseColor: '{primary.50}',
+          hoverColor: '{primary.950}',
+          activeColor: '{primary.400}',
+        },
+        highlight: {
+          background: '{primary.500}',
+          focusBackground: '{primary.700}',
+          color: '#ffffff',
+          focusColor: '#ffffff',
+        },
+      },
+      dark: {
+        primary: {
+          color: '{primary.50}',
+          inverseColor: '{primary.950}',
+          hoverColor: '{primary.100}',
+          activeColor: '{primary.200}',
+        },
+        highlight: {
+          background: 'rgba(250, 250, 250, .16)',
+          focusBackground: 'rgba(250, 250, 250, .24)',
+          color: 'rgba(255,255,255,.87)',
+          focusColor: 'rgba(255,255,255,.87)',
+        },
+      },
+    },
+  },
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -42,5 +93,13 @@ export const appConfig: ApplicationConfig = {
     provideAccountConfig(),
     provideTenantManagementConfig(),
     provideAbpThemeShared(),
-  ]
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: false || 'none',
+        },
+      },
+    }),
+  ],
 };
--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,17 +1,29 @@
 import { Component, inject } from '@angular/core';
 import { AuthService, LocalizationPipe } from '@abp/ng.core';
+import { ButtonModule } from 'primeng/button';
+import { Table, TableModule } from 'primeng/table';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [LocalizationPipe]
+  imports: [LocalizationPipe, ButtonModule, TableModule, AccordionModule],
+  standalone: true,
 })
 export class HomeComponent {
   private authService = inject(AuthService);
 
+  products = [
+    { code: '1', name: 'Product 1', category: 'Category 1', quantity: 1 },
+    { code: '2', name: 'Product 2', category: 'Category 2', quantity: 2 },
+    { code: '3', name: 'Product 3', category: 'Category 3', quantity: 3 },
+    { code: '4', name: 'Product 4', category: 'Category 4', quantity: 4 },
+    { code: '5', name: 'Product 5', category: 'Category 5', quantity: 5 },
+  ];
+
   get hasLoggedIn(): boolean {
-    return this.authService.isAuthenticated
+    return this.authService.isAuthenticated;
   }
 
   login() {
